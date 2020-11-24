b78a146ee67aaa93b8567b6e4441fcc2ab4fbd49
qiyulei <qiyulei@gridsum.com>
2020-11-24 18:44:32 +0800

    feat: 初次提交

---
+++ packages/ui-app/src/index.ts
@@ -0,0 +1,6 @@
+import { add } from 'ui-lib';
+
+function main(){
+  console.log(add(1,2));
+  console.log('app')
+}
---
+++ packages/ui-lib/src/index.ts
@@ -0,0 +1,7 @@
+import lodash from "lodash";
+export function add(a: number, b: number) {
+  return lodash.add(a,b);
+}
+export function reduce(a: number, b: number) {
+  return lodash.reduce(a,b);
+}
